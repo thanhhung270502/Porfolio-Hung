eb3ea824008efaddf5d37b422a413c2c15aaf12d
thanhhung270502 <thanhhung270502@gmail.com>
2023-03-08 23:13:05 +0700

    Update project + Up home & education

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -4,28 +4,41 @@ const $$ = document.querySelectorAll.bind(document);
 const toggleSwitch = $('.switch__darkmode');
 const currentTheme = localStorage.getItem('theme');
 const darkmode = $('.darkmode');
+const body = $('body');
+
+const mobile__toggle = $('.navbar-toggler');
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'dark') {
         darkmode.classList.remove('uil-moon')
         darkmode.classList.add('uil-sun')
+        darkmode.style.color = 'var(--yellow)';
+    }
+    else {
+        darkmode.style.color = 'var(--blue-dark)';
     }
 }
 
 toggleSwitch.onclick = () => {
-    if (darkmode.classList.contains('uil-moon')) {
+    if (darkmode.classList.contains('uil-sun')) {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
 
-        darkmode.classList.remove('uil-moon')
-        darkmode.classList.add('uil-sun')
+        darkmode.classList.remove('uil-sun')
+        darkmode.classList.add('uil-moon')
+        darkmode.style.color = 'var(--blue-dark)';
+
+        mobile__toggle.style.backgroundColor = 'unset';
     }
     else {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
 
-        darkmode.classList.remove('uil-sun')
-        darkmode.classList.add('uil-moon')
+        darkmode.classList.remove('uil-moon')
+        darkmode.classList.add('uil-sun')
+        darkmode.style.color = 'var(--yellow)';
+        
+        mobile__toggle.style.backgroundColor = 'var(--yellow)';
     }
 }
